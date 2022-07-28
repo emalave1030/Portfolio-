ed66daa56c1b6ea1c78dd9d8ce2eff3bf01d7825
Erwin Malave <76267760+emalave1030@users.noreply.github.com>
2022-07-28 19:09:12 -0400

    Add files via upload

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <a href='https://www.linkedin.com/in/erwin-malave-89a20291'>
+    <div>
+      <BsLinkedin />
+    </div>
+    </a>
+    
+  </div>
+);
+
+export default SocialMedia;
---
+++ src/container/Skills/Skills.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect} from 'react';
+import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+import './Skills.scss';
+
+
+
+
+const Skills = () => {
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query)
+    .then((data) => {
+     setExperience(data);
+                                                                   
+    })
+
+    client.fetch(skillsQuery)
+    .then((data) => {
+     setSkills(data);
+                                                                   
+    })
+}, [])
+
+
+  return (
+    <>
+    <h2 className="head-text">Skills </h2>
+    <p>I've worked with a range of technologies in the web development and computer science field including Back-End Databases, Front-End Design Tools, Blockchain Databases, Smart Contracts and 3D Animation.</p>
+
+    <div className="app__skills-container">
+      <motion.div className="app__skills-list">
+      {skills.map((skill) => (
+        <motion.div
+        whileInView={{opacity: [0, 1]}}
+        transition={{ duration: 0.5 }}
+        className="app__skills-item app__flex"
+        key={skill.name}
+        >
+            <div className="app__flex" style={{ backgroundColor: skill.bgColor}}>
+              <img src={urlFor(skill.icon)} alt={skill.name}/>
+            </div>
+            <p className="p-text">{skill.name}</p>
+        </motion.div>
+      ))}
+      </motion.div>
+    </div>
+    </>
+  )
+}
+
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'),
+  'skills',
+  'app__whitebg',
+);
